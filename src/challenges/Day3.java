09bc3e8e77d52603e4633aa883fa007ae73d201e
Maya Messinger <maya.messinger@gmail.com>
2021-12-07 20:34:27 -0600

    Day 3: part 2

--- src/challenges/Day3.java
+++ src/challenges/Day3.java
@@ -4,22 +4,22 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Day3 {
     public static void main(String[] args) {
         String fileName = "resources/day3.txt";
-        int[] oneOccurrences = null;
-        int totalLines = 0;
 
+        List<String> allLines = new ArrayList<>() {};
+        HashMap<Integer, List<String>> linesWithOneAtKeyIndex = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = br.readLine()) != null) {
-                if (oneOccurrences == null)
-                    oneOccurrences = new int[line.length()];
+                allLines.add(line);
 
-                updateOccurrences(oneOccurrences, line);
-
-                totalLines++;
+                updateOccurrences(linesWithOneAtKeyIndex, line);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -27,43 +27,52 @@ public class Day3 {
             e.printStackTrace();
         }
 
-        Rates rates = calculateGammaRate(oneOccurrences, totalLines);
+        int oxygenGeneratorRating = calculateRating(linesWithOneAtKeyIndex, allLines, true);
+        int co2GeneratorRating = calculateRating(linesWithOneAtKeyIndex, allLines, false);
 
-        System.out.println(rates.gammaRate * rates.epsilonRate);
+        System.out.println(oxygenGeneratorRating * co2GeneratorRating);
     }
 
-    private static void updateOccurrences(int[] occurrences, String line) {
+    private static void updateOccurrences(HashMap<Integer, List<String>> numbersWithOneAtKeyIndex, String line) {
         for (var i = 0; i < line.length(); i++) {
-            if (line.charAt(i) == '1')
-                occurrences[i] += 1;
+            if (line.charAt(i) == '1') {
+                if (numbersWithOneAtKeyIndex.get(i) != null) {
+                    numbersWithOneAtKeyIndex.get(i).add(line);
+                }
+                else {
+                    numbersWithOneAtKeyIndex.put(i, new ArrayList<>());
+                    numbersWithOneAtKeyIndex.get(i).add(line);
+                }
+            }
         }
     }
 
-    private static Rates calculateGammaRate(int[] occurrences, int totalLines) {
-        String gammaRate = "";
-        String epsilonRate = "";
-
-        for (int i = 0; i < occurrences.length; i++) {
-            if (occurrences[i] > totalLines / 2) {
-                gammaRate += "1";
-                epsilonRate += "0";
+    private static int calculateRating(
+        HashMap<Integer, List<String>> linesWithValueAtKeyIndex,
+        List<String> allLines,
+        boolean retainMostCommon
+    ) {
+        List<String> filteredLines = new ArrayList<>(allLines);
+        for (int i = 0; i < linesWithValueAtKeyIndex.keySet().size(); i++) {
+            List<String> numbersWithValueAtI = new ArrayList<>(linesWithValueAtKeyIndex.get(i));
+            numbersWithValueAtI.retainAll(filteredLines);
+            if (numbersWithValueAtI.size() >= (filteredLines.size() + 1) / 2) {
+                if (retainMostCommon)
+                    filteredLines.retainAll(numbersWithValueAtI);
+                else
+                    filteredLines.removeAll(numbersWithValueAtI);
             }
             else {
-                gammaRate += "0";
-                epsilonRate += "1";
+                if (retainMostCommon)
+                    filteredLines.removeAll(numbersWithValueAtI);
+                else
+                    filteredLines.retainAll(numbersWithValueAtI);
             }
+
+            if (filteredLines.size() == 1)
+                break;
         }
 
-        return new Rates(Integer.parseInt(gammaRate, 2), Integer.parseInt(epsilonRate, 2));
-    }
-
-    public static class Rates {
-        int gammaRate;
-        int epsilonRate;
-
-        public Rates(int gammaRate, int epsilonRate) {
-            this.gammaRate = gammaRate;
-            this.epsilonRate = epsilonRate;
-        }
+        return Integer.parseInt(filteredLines.get(0), 2);
     }
 }
