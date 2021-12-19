b08aeeff201c189723e1fde43ee0af74da5cfbb4
Maya Messinger <maya.messinger@gmail.com>
2021-12-18 20:25:12 -0600

    Day 15: part 2

--- src/challenges/Day15.java
+++ src/challenges/Day15.java
@@ -51,9 +51,10 @@ public class Day15 {
     }
 
     private static int lowestRiskPathCost(int[][] positionRisks) {
-        Set<DijkstraNode> positions = createNodeSet(positionRisks);
-        DijkstraNode start = getNodeFromXY(positions, 0, 0);
-        DijkstraNode end = getNodeFromXY(positions, positionRisks.length - 1, positionRisks[0].length - 1);
+        DijkstraNode[][] positions = createNodeArray(positionRisks);
+        positions = getExpandedGrid(positions);
+        DijkstraNode start = positions[0][0];
+        DijkstraNode end = positions[positions.length - 1][positions[0].length - 1];
 
         runDijkstraAlgorithm(positions, start, end);
 
@@ -66,27 +67,28 @@ public class Day15 {
         return matching.size() == 1 ? matching.get(0) : null;
     }
 
-    private static Set<DijkstraNode> createNodeSet(int[][] positionRisks) {
-        Set<DijkstraNode> nodes = new HashSet<>();
+    private static DijkstraNode[][] createNodeArray(int[][] positionRisks) {
+        DijkstraNode[][] nodes = new DijkstraNode[positionRisks.length][positionRisks[0].length];
 
         for (int i = 0; i < positionRisks.length; i++) {
             for (int j = 0; j < positionRisks[i].length; j++) {
-                nodes.add(new DijkstraNode(i, j, positionRisks[i][j]));
+                nodes[i][j] = new DijkstraNode(i, j, positionRisks[i][j]);
             }
         }
 
         return nodes;
     }
 
-    private static void runDijkstraAlgorithm(Set<DijkstraNode> nodes, DijkstraNode start, DijkstraNode end) {
-        Set<DijkstraNode> unvisitedNodes = new HashSet<>(nodes);
+    private static void runDijkstraAlgorithm(DijkstraNode[][] nodes, DijkstraNode start, DijkstraNode end) {
+        Set<DijkstraNode> unvisitedNodes = new HashSet<>();
 
         start.setCost(0);
         unvisitedNodes.remove(start);
 
         DijkstraNode current = start;
-        while (unvisitedNodes.contains(end)) {
+        while (!end.getIsVisited()) {
             Set<DijkstraNode> neighbors = getNeighbors(nodes, current);
+            unvisitedNodes.addAll(neighbors.stream().filter(n -> !n.getIsVisited()).toList());
             for (DijkstraNode neighbor : neighbors) {
                 int potentialCost = current.getCost() + neighbor.getRisk();
                 if (neighbor.getCost() > potentialCost)
@@ -94,11 +96,12 @@ public class Day15 {
             }
 
             unvisitedNodes.remove(current);
+            current.visit();
             current = bestUnvisitedNode(unvisitedNodes);
         }
     }
 
-    private static Set<DijkstraNode> getNeighbors(Set<DijkstraNode> nodes, DijkstraNode node) {
+    private static Set<DijkstraNode> getNeighbors(DijkstraNode[][] nodes, DijkstraNode node) {
         Set<DijkstraNode> neighbors = new HashSet<>();
 
         int[][] possibleNeighborXYs = new int[][] {
@@ -109,9 +112,11 @@ public class Day15 {
         };
 
         for (int[] neighborCoordinate : possibleNeighborXYs) {
-            DijkstraNode possibleNeighbor = getNodeFromXY(nodes, neighborCoordinate[0], neighborCoordinate[1]);
-            if (possibleNeighbor != null)
+            try {
+                DijkstraNode possibleNeighbor = nodes[neighborCoordinate[0]][neighborCoordinate[1]];
                 neighbors.add(possibleNeighbor);
+            }
+            catch (IndexOutOfBoundsException e) {}
         }
 
         return neighbors;
@@ -119,6 +124,78 @@ public class Day15 {
 
     private static DijkstraNode bestUnvisitedNode(Set<DijkstraNode> unvisited) {
         return unvisited.stream().min(Comparator.comparingInt(DijkstraNode::getCost)).orElse(null);
+    }
+
+    private static DijkstraNode[][] getExpandedGrid(DijkstraNode[][] grid) {
+        DijkstraNode[][] expandedGrid = new DijkstraNode[grid.length * 5][grid[0].length * 5];
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                for (int k = 1; k < 5; k++) {
+                    expandedGrid[i][j] = grid[i][j];
+                    addRight(expandedGrid, i, j, k, grid[i][j]);
+                    addRightBelow(expandedGrid, i, j, k, grid[i][j]);
+                    addBelow(expandedGrid, i, j, k, grid[i][j]);
+                    addBelowRight(expandedGrid, i, j, k, grid[i][j]);
+                    addDiagonal(expandedGrid, i, j, k, grid[i][j]);
+                }
+            }
+        }
+
+        return expandedGrid;
+    }
+
+    private static void addRight(DijkstraNode[][] expandedGrid, int x, int y, int layer, DijkstraNode node) {
+        int dimension = expandedGrid.length / 5;
+        int newY = layer * dimension + y;
+        int newRisk = (node.getRisk() + layer) > 9
+            ? (node.getRisk() + layer) - 9
+            : (node.getRisk() + layer);
+        expandedGrid[x][newY] = new DijkstraNode(x, newY, newRisk);
+    }
+
+    private static void addRightBelow(DijkstraNode[][] expandedGrid, int x, int y, int layer, DijkstraNode node) {
+        int dimension = expandedGrid.length / 5;
+        for (int i = 1; i < layer; i++) {
+            int newX = i * dimension + x;
+            int newY = layer * dimension + y;
+            int newRisk = (node.getRisk() + layer + i) > 9
+                ? (node.getRisk() + layer + i) - 9
+                : (node.getRisk() + layer + i);
+            expandedGrid[newX][newY] = new DijkstraNode(newX, newY, newRisk);
+        }
+    }
+
+    private static void addBelow(DijkstraNode[][] expandedGrid, int x, int y, int layer, DijkstraNode node) {
+        int dimension = expandedGrid.length / 5;
+        int newX = layer * dimension + x;
+        int newRisk = (node.getRisk() + layer) > 9
+            ? (node.getRisk() + layer) - 9
+            : (node.getRisk() + layer);
+        expandedGrid[newX][y] = new DijkstraNode(newX, y, newRisk);
+    }
+
+    private static void addBelowRight(DijkstraNode[][] expandedGrid, int x, int y, int layer, DijkstraNode node) {
+        int dimension = expandedGrid.length / 5;
+        for (int i = 1; i < layer; i++) {
+            int newX = layer * dimension + x;
+            int newY = i * dimension + y;
+            int newRisk = (node.getRisk() + layer + i) > 9
+                    ? (node.getRisk() + layer + i) - 9
+                    : (node.getRisk() + layer + i);
+            expandedGrid[newX][newY] = new DijkstraNode(newX, newY, newRisk);
+        }
+    }
+
+    private static void addDiagonal(DijkstraNode[][] expandedGrid, int x, int y, int layer, DijkstraNode node) {
+        int dimension = expandedGrid.length / 5;
+        int newX = layer * dimension + x;
+        int newY = layer * dimension + y;
+        int newRisk = (node.getRisk() + layer * 2) > 9
+            ? (node.getRisk() + layer * 2) - 9
+            : (node.getRisk() + layer * 2);
+
+        expandedGrid[newX][newY] = new DijkstraNode(newX, newY, newRisk);
     }
 }
 
@@ -135,6 +212,10 @@ class DijkstraNode {
     private int costToVisit;
     public int getCost() { return costToVisit; }
     public void setCost(int cost) { costToVisit = cost; }
+
+    private boolean isVisited;
+    public boolean getIsVisited() { return isVisited; }
+    public void visit() { isVisited = true; }
 
     public int hashCode() {
         return xValue * 7919 + yValue * 5897;
